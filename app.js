9b95fde1f0933ad0e5a36135f170fc37bd8cee0c
Dongare <akshay.dongare@capgemini.com>
2019-04-22 14:57:58 +0530

    cors and json middleware changes

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var express = require("express");
 var app = express();
+var cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
 
 var inventory = require("./inventory");
 app.use("/inventory",inventory);
--- inventory.js
+++ inventory.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 var db = require("./db");
 
-router.get("/listItems",function(req,res){
+router.get("/items",function(req,res){
   var pool = db.getConnection();
   pool.query("select * from items" , function(err,data){
     if(err!=undefined)
